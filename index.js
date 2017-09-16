492e0c0d98cecc1437d44ea84cf26f756cf4dbdd
Jannis R <mail@jannisr.de>
2017-09-16 16:20:59 +0200

    measurements GET api

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const corser = require('corser')
 const boom = require('boom')
+const ndjson = require('ndjson')
+const pump = require('pump')
+const sink = require('stream-sink')
 
 const db = require('./db')
 
 const app = express()
 
-letsencrypt
-.create({
-	server: 'https://acme-v01.api.letsencrypt.org/directory',
-	agreeTos: true,
-	email: process.env.EMAIL,
-	approveDomains: ['cyclehack-2017-lora-backend.jannisr.de'],
-	app
-})
-.listen(80, 443)
+// letsencrypt
+// .create({
+// 	server: 'https://acme-v01.api.letsencrypt.org/directory',
+// 	agreeTos: true,
+// 	email: process.env.EMAIL,
+// 	approveDomains: ['cyclehack-2017-lora-backend.jannisr.de'],
+// 	app
+// })
+// .listen(process.env.HTTP_PORT || 80, process.env.HTTPS_PORT || 443)
 
 app.use(corser.create())
 app.use(bodyParser.json())
 
 app.post('/measurements', (req, res, next) => {
 	if (! req.body.metadata) {
-		next(boom.badRequest('metadata is missing'))
+		next(boom.badRequest('regpsifjwofij is missing'))
 		return
 	}
 
@@ -43,6 +46,26 @@ app.post('/measurements', (req, res, next) => {
 	})
 })
 
+app.get('/measurements', (req, res, next) => {
+	db.createValueStream({limit: 100})
+	.pipe(sink.object())
+	.then((vals) => {
+		res.json(vals)
+	})
+	.catch(next)
+})
+
+app.get('/measurements/all', (req, res, next) => {
+	pump(
+		db.createValueStream(),
+		ndjson.serialize(),
+		res,
+		next
+	)
+})
+
 app.use((err, req, res, next) => {
 	res.status(err.output.statusCode || 500).send(err.message || 'Unknown error')
 })
+
+app.listen(process.env.HTTP_PORT || 80)
